e5eb9fe4d2f820170a19a89ac1aedd26d9f5e00a
qrtx-Nami <editsneyst@gmail.com>
2025-10-04 22:19:29 +0300

    Create calculatorpromax.0.1.cpp

---
+++ calculator 0.1/calculatorpromax.0.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+
+
+using namespace std;
+ int main(){
+ cout<<"калькулятор. мега крутая программа ваще топчик. все права защищены навсегда кто украдет того суд"<<endl;
+
+
+ double a, b;
+ cout<<"Первое число:";
+ cin>>a;
+ cout<<"Второе число:";
+ cin>>b;
+  cout<<"Результат вычисления:"<<endl;
+    cout<<a<<"+"<<b<<"="<<a + b<<endl;
+    cout<<a<<"-"<<b<<"="<<a - b<<endl;
+    cout<<a<<"*"<<b<<"="<<a * b<<endl;
+    cout<<a<<"/"<<b<<"="<<a / b<<endl;
+ 
+ }
